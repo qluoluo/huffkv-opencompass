4d733484adcd38a2a2bc212cf2d57d22f1a28fa7
qluoluo <745047569@qq.com>
2025-08-21 07:24:41 +0800

    hzwproject-init

--- eval_script/25.8.20/Llama-3_2_3B-taylorkv/eval-niah.sh
+++ eval_script/25.8.20/Llama-3_2_3B-taylorkv/eval-niah.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # root_path=/inspire/hdd/ws-8207e9e2-e733-4eec-a475-cfa1c36480ba/embodied-multimodality/liuxiaoran-240108120089
-source /inspire/hdd/project/embodied-multimodality/liuxiaoran-240108120089/public/resurrection.sh
+# source /inspire/hdd/project/embodied-multimodality/liuxiaoran-240108120089/public/resurrection.sh
 
-root_path=/inspire/hdd/project/embodied-multimodality/liuxiaoran-240108120089
+# root_path=/inspire/hdd/project/embodied-multimodality/liuxiaoran-240108120089
+root_path=/inspire/hdd/project/heziweiproject/liuxiaoran-240108120089
 source ${root_path}/local_conda.sh
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)
@@ -10,7 +11,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd -P)
 
 echo "### start eval ###"
 conda activate zgliu-huffkv-opencompass
-cd ${root_path}/projects_zgliu/projects/huffKV/opencompass
+cd ${root_path}/projects_zgliu/projects/huffkv/huffkv-opencompass
 sleep 3
 opencompass ${script_dir}/opencompass-eval-niah.py -w ${script_dir}/oc-eval-result/niah -r
 sleep 30
